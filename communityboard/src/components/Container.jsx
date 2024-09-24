7823d422ca6ba52236df5d2735b329a3a437988b
Carlos Sac <carlos.sac@outlook.com>
2024-09-24 01:47:50 -0400

    Add display sizes support

--- communityboard/src/components/Container.jsx
+++ communityboard/src/components/Container.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import Event from "./Event";
+import image1 from '../assets/cable_car.jpg'; 
+import image2 from '../assets/bridge.jpeg';
+import image3 from '../assets/clerigos-tower.jpg';
+import image4 from '../assets/bistro.jpeg';
 import "../App.css";
 
 const Container = () => {
     return (
         <div className='Container'>
             <Event
-                image='https://via.placeholder.com/150'
+                image={image1}                
                 title='Title 1'
                 link='https://www.example.com'
             />
             <Event
-                image='https://via.placeholder.com/150'
+                image={image2}
                 title='Title 1'
                 link='https://www.example.com'
             />
             <Event
-                image='https://via.placeholder.com/150'
+                image={image3}
                 title='Title 1'
                 link='https://www.example.com'
             />
             <Event
-                image='https://via.placeholder.com/150'
+                image={image4}
                 title='Title 1'
                 link='https://www.example.com'
             />
